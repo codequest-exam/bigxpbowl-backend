72fa42862d6d4fedbda04365ed6c643a60e15bae
Jaes98 <marcus.jaes@gmail.com>
2024-05-13 11:56:49 +0200

    init

---
+++ src/main/java/kea/exam/xpbowlingbackend/XpbowlingBackendApplication.java
@@ -0,0 +1,13 @@
+package kea.exam.xpbowlingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpbowlingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpbowlingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exam/xpbowlingbackend/XpbowlingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exam.xpbowlingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpbowlingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
