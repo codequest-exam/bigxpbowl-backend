b5104aba6ea01c8afd6a15999370710f5b855ff1
buan0001 <bustermikkel@gmail.com>
2024-05-28 14:02:14 +0200

    Basic shift controller

---
+++ src/main/java/kea/exam/xpbowlingbackend/staff/ShiftService.java
@@ -0,0 +1,49 @@
+package kea.exam.xpbowlingbackend.staff;
+
+import org.aspectj.weaver.ast.Instanceof;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ShiftService {
+    private final ShiftRepository shiftRepository;
+    private final StaffRepository staffRepository;
+
+    public ShiftService(ShiftRepository shiftRepository, StaffRepository staffRepository) {
+        this.shiftRepository = shiftRepository;
+        this.staffRepository = staffRepository;
+    }
+
+    public List<Shift> getShifts() {
+        return shiftRepository.findAll();
+    }
+
+    public Shift getShift(int id) {
+        return shiftRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Shift not found"));
+    }
+
+    public Shift changeStaff(int id, int staffId) {
+        Shift shift = getShift(id);
+
+        if (staffRepository.existsById(staffId)) {
+            if (!shift.getStaff().isEmpty() && isStaffAssigned(shift, staffId)) {
+                shift.getStaff().removeIf(staff -> staff.getId() == staffId);
+            }
+            else {
+                Staff staff = staffRepository.findById(staffId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Staff not found"));
+                shift.getStaff().add(staff);
+            }
+            return shiftRepository.save(shift);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Staff not found");
+        }
+    }
+
+    private boolean isStaffAssigned(Shift shift, int staffId) {
+        return shift.getStaff().stream().anyMatch(staff -> staff.getId() == staffId);
+    }
+}
